b8d824e75301115b3c8276cb35ef341bd2597ecd
davidfeng <davidfeng.8844@gmail.com>
2023-12-23 22:59:42 +0800

    update the vdot agorithm

--- Runners' Hub/js/main.js
+++ Runners' Hub/js/main.js
@@ -173,7 +173,7 @@ function deg2rad(deg) {
 
 
 
-//JQeury Start here
+//JQeury
 $(document).ready(function () {
     //Update messages
     $('#updateModal').modal('show');
@@ -220,11 +220,16 @@ $("#generatePlan").click(function() {
     let weekDistance = $("#weekDistance").val();
     //convert weekDistance to km according to the user's choice
     let distanceType = $("#distanceType").val();
-    let personalBestType = $("#personalBestType").val();
+    //For the personalBest's inner HTML
+    const personalBestSelect = document.getElementById("personalBestType");
+    const pbselectedIndex = personalBestSelect.selectedIndex;
+    const personalBestType = personalBestSelect.options[pbselectedIndex].innerHTML;
     // For the planDistance's inner HTML
     const plannedDistanceSelect = document.getElementById("plannedDistance");
     const selectedIndex = plannedDistanceSelect.selectedIndex;
     const plannedDistanceType = plannedDistanceSelect.options[selectedIndex].innerHTML;
+    //calculate the VO2max
+    let vdot = findVDOT(personalBest, personalBestType);
 
     // Check if weeks is a valid number
     if (isNaN(weeks) || weeks <= 0) {
@@ -238,6 +243,7 @@ $("#generatePlan").click(function() {
     <h2 style="text-align: center; color: #0a0c0f;">${plannedDistanceType} Training Plan</h2>
     <p><strong>Weeks:</strong> ${weeks}</p>
     <p><strong>Personal Best:</strong> ${personalBest} mins</p>
+    <p><strong>VO2max:</strong> ${vdot}</p> 
     <!-- Include additional plan details here -->
     `;
     if (distanceType === "km") {
@@ -267,6 +273,28 @@ $("#generatePlan").click(function() {
         }
         content += '</tr>';
     }
+    content += '</tbody></table>';
+    // The VDOT table Explanation
+    content += '<h2 style="text-align: center; color: #0a0c0f;">VDOT Table Explanation</h2>';
+    content += '<p style="text-align: justify; color: #0a0c0f;">The VDOT table is used to calculate the training paces for each training activity. The VDOT value is calculated based on the personal best time and the distance of the personal best. The VDOT value is then used to calculate the training paces for each training activity.</p>';
+
+    // The VDOT table
+    content += '<h2 style="text-align: center; color: #0a0c0f;">VDOT Table</h2>';
+    content += '<table style="margin: 0 auto; border-collapse: collapse; width: 80%; margin-top: 20px;" border="1">';
+    content += '<thead style="background-color: #0a0c0f; color: #fff;"><tr><th style="padding: 10px;">VDOT</th><th style="padding: 10px;">5K</th><th style="padding: 10px;">10K</th><th style="padding: 10px;">Half Marathon</th><th style="padding: 10px;">Marathon</th></tr></thead>';
+    content += '<tbody>';
+    content += '<tr><td>30</td><td>30</td><td>63</td><td>140</td><td>290</td></tr>';
+    content += '<tr><td>35</td><td>27</td><td>56</td><td>126</td><td>256</td></tr>';
+    content += '<tr><td>40</td><td>24</td><td>77</td><td>110</td><td>229</td></tr>';
+    content += '<tr><td>45</td><td>22</td><td>45</td><td>100</td><td>208</td></tr>';
+    content += '<tr><td>50</td><td>20</td><td>41</td><td>91</td><td>190</td></tr>';
+    content += '<tr><td>55</td><td>18</td><td>38</td><td>84</td><td>176</td></tr>';
+    content += '<tr><td>60</td><td>17</td><td>35</td><td>78</td><td>163</td></tr>';
+    content += '<tr><td>65</td><td>16</td><td>33</td><td>72</td><td>151</td></tr>';
+    content += '<tr><td>70</td><td>15</td><td>31</td><td>68</td><td>141</td></tr>';
+    content += '<tr><td>75</td><td>14</td><td>29</td><td>64</td><td>132</td></tr>';
+    content += '<tr><td>80</td><td>13</td><td>27</td><td>60</td><td>124</td></tr>';
+    content += '<tr><td>85</td><td>12</td><td>26</td><td>57</td><td>117</td></tr>';
     content += '</tbody></table>';
     content += '</body>'
 
@@ -306,13 +334,47 @@ function getTrainingActivity(day) {
     }
 }
 // function to calculate the VO2max based on the personal best
-function getVO2max(personalBest, personalBestType) {
-    if (personalBestType === "km") {
-        return personalBest / 3.5;
-    } else if (personalBestType === "miles") {
-        return personalBest / 1.609;
+function findVDOT(time, distance) {
+    //VDOT data
+    const vdotData = [
+      [30, 30, 63, 140, 290],
+      [35, 27, 56, 126, 256],
+      [40, 24, 77, 110, 229],
+      [45, 22, 45, 100, 208],
+      [50, 20, 41, 91, 190],
+      [55, 18, 38, 84, 176],
+      [60, 17, 35, 78, 163],
+      [65, 16, 33, 72, 151],
+      [70, 15, 31, 68, 141],
+      [75, 14, 29, 64, 132],
+      [80, 13, 27, 60, 124],
+      [85, 12, 26, 57, 117]
+    ];  
+
+    // Find the index of the distance column
+    const distanceIndex = ['5K', '10K', 'Half Marathon', 'Marathon'].indexOf(distance);
+  
+    // If the distance is not valid, return an error message
+    if (distanceIndex === -1) {
+      return "Invalid distance";
     }
-}
+  
+    // Find the row in the VDOT data with the closest time in the specified distance column
+    const closestRow = vdotData.reduce((closest, entry) => {
+      const entryTime = entry[distanceIndex + 1]; // Adjusting the index to the selected distance column
+      const currentDiff = Math.abs(entryTime - time);
+      const closestDiff = Math.abs(closest[distanceIndex + 1] - time);
+  
+      return currentDiff < closestDiff ? entry : closest;
+    });
+  
+    // Return the VDOT value from the closest row
+    return closestRow[0];
+  }
+  
+  //const vdotValue = findVDOT(38, '10K');
+  //console.log(`VDOT Value: ${vdotValue}`);
+  
 });
 
 
