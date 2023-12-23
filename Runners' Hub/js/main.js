ff7987018c8dd965b81c758d635b097fe865f9df
davidfeng <davidfeng.8844@gmail.com>
2023-12-23 18:40:43 +0800

    Update the location button

--- Runners' Hub/js/main.js
+++ Runners' Hub/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         const currentDistance = getElementValue('currentDistance');
         const distanceType = getElementValue('distanceType');
-        const personalBest = getElementValue('PersonalBest');
+        const personalBest = getElementValue('personalBest');
         const personalBestType = getElementValue('personalBestType');
         const plannedDistance = getElementValue('plannedDistance');
         const weeks = getElementValue('weeks');
@@ -50,7 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
                         element.value = value || '';
                     } else if (element.tagName === 'CHECKBOX') {
                         // Special handling for checkboxes
-                        element.checked = value === true;
+                        element.checked = value = true;
                     }
                 }
             });
@@ -91,6 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 //Get user geographic location
+/*
 function getLocation() {
     document.getElementById("demoLocation").classList.toggle("d-none");
     if (navigator.geolocation) {
@@ -103,15 +104,80 @@ function getLocation() {
 
 function showPosition(position) {
     document.getElementById("demoLocation").innerHTML = "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;  
+        "<br>Longitude: " + position.coords.longitude;
+}
+*/
+function getLocation() {
+    document.getElementById("demoLocation").classList.toggle("d-none");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("demoLocation").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    const userLatitude = position.coords.latitude;
+    const userLongitude = position.coords.longitude;
+
+    // Coordinates of the specified cities
+    const cities = [
+        { name: "Boston", latitude: 42.3601, longitude: -71.0589 },
+        { name: "New York", latitude: 40.7128, longitude: -74.0060 },
+        { name: "Berlin", latitude: 52.5200, longitude: 13.4050 },
+        { name: "Tokyo", latitude: 35.6895, longitude: 139.6917 },
+        { name: "Chicago", latitude: 41.8781, longitude: -87.6298 },
+        { name: "London", latitude: 51.5074, longitude: -0.1278 }
+    ];
+
+    // Calculate distances
+    const distances = cities.map(city => {
+        const dLat = deg2rad(city.latitude - userLatitude);
+        const dLon = deg2rad(city.longitude - userLongitude);
+        const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+                  Math.cos(deg2rad(userLatitude)) * Math.cos(deg2rad(city.latitude)) *
+                  Math.sin(dLon / 2) * Math.sin(dLon / 2);
+        const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        const distance = 6371 * c; // Radius of Earth in kilometers
+        return { name: city.name, distance };
+    });
+
+    // Find the nearest city
+    const nearestCity = distances.reduce((min, current) => (min.distance < current.distance) ? min : current);
+
+    // Coordinates of continents bounding boxes
+    const continents = [
+        { name: "North America", minLat: 24.396308, maxLat: 71.538800, minLon: -125.000000, maxLon: -66.934570 },
+        { name: "Europe", minLat: 36.673900, maxLat: 71.538800, minLon: -25.000000, maxLon: 45.000000 },
+        { name: "Asia", minLat: -11.000000, maxLat: 71.538800, minLon: 25.000000, maxLon: 180.000000 },
+        // Add more continents as needed
+    ];
+
+    // Detect continent
+    const detectedContinent = continents.find(continent =>
+        userLatitude >= continent.minLat &&
+        userLatitude <= continent.maxLat &&
+        userLongitude >= continent.minLon &&
+        userLongitude <= continent.maxLon
+    );
+
+    document.getElementById("demoLocation").innerHTML =
+        "Your location:<br>Latitude: " + userLatitude + "<br>Longitude: " + userLongitude + (detectedContinent ? "<br>Continent: " + detectedContinent.name : "<br>Continent not detected") + 
+        "<br>Nearest Major city Marathon: " + nearestCity.name + " (" + nearestCity.distance.toFixed(2) + " km away)";
+}
+
+// Helper function to convert degrees to radians
+function deg2rad(deg) {
+    return deg * (Math.PI / 180);
 }
 
 
 
+//JQeury Start here
 $(document).ready(function () {
     //Update messages
     $('#updateModal').modal('show');
-    
+
     //Make gears-intro fade out on scroll
     $(window).scroll(function () {
         $(".gears-jumbotron").css("opacity", 1 - $(window).scrollTop() / 500);
@@ -144,15 +210,18 @@ $(document).ready(function () {
       let windowHeight = $(window).height();
       let progress = (scrollPosition / (documentHeight - windowHeight)) * 100;
       progressBarInner.width(progress + '%');
-    }); 
+    });
 
-    //Generate plan
+//Generate plan
 $("#generatePlan").click(function() {
     // Get user inputs
     let weeks = $("#weeks").val();
     let personalBest = $("#personalBest").val();
-    let age = $("#age").val();
     let location = $("#demoLocation").val();
+    // For the Event's inner HTML
+    const plannedDistanceSelect = document.getElementById("plannedDistance");
+    const selectedIndex = plannedDistanceSelect.selectedIndex;
+    const plannedDistanceType = plannedDistanceSelect.options[selectedIndex].innerHTML;
 
     // Check if weeks is a valid number
     if (isNaN(weeks) || weeks <= 0) {
@@ -162,17 +231,17 @@ $("#generatePlan").click(function() {
 
     // Process data and generate advanced plan content
     let content = `
-    <h2 style="text-align: center; color: #007bff;">Advanced Marathon Training Plan</h2>
+    <body style="background-color: #1393b6; padding: 20px; border-radius: 10px;">
+    <h2 style="text-align: center; color: #0a0c0f;">${plannedDistanceType} Training Plan</h2>
     <p><strong>Weeks:</strong> ${weeks}</p>
     <p><strong>Personal Best:</strong> ${personalBest}</p>
-    <p><strong>Age:</strong> ${age}</p>
     <p><strong>Location:</strong> ${location}</p>
     <!-- Include additional plan details here -->
     <p><strong>Generated on:</strong> ${new Date().toLocaleDateString()}</p>
     `;
-    content += '<h2 style="text-align: center; color: #007bff;">Training Plan</h2>';
+    content += '<h2 style="text-align: center; color: #0a0c0f;">Training Plan</h2>';
     content += '<table style="margin: 0 auto; border-collapse: collapse; width: 80%; margin-top: 20px;" border="1">';
-    content += '<thead style="background-color: #007bff; color: #fff;"><tr><th style="padding: 10px;">Week</th><th style="padding: 10px;">Day 1</th><th style="padding: 10px;">Day 2</th><th style="padding: 10px;">Day 3</th><th style="padding: 10px;">Day 4</th><th style="padding: 10px;">Day 5</th><th style="padding: 10px;">Day 6</th><th style="padding: 10px;">Day 7</th></tr></thead>';
+    content += '<thead style="background-color: #0a0c0f; color: #fff;"><tr><th style="padding: 10px;">Week</th><th style="padding: 10px;">Day 1</th><th style="padding: 10px;">Day 2</th><th style="padding: 10px;">Day 3</th><th style="padding: 10px;">Day 4</th><th style="padding: 10px;">Day 5</th><th style="padding: 10px;">Day 6</th><th style="padding: 10px;">Day 7</th></tr></thead>';
     content += '<tbody>';
 
     for (let week = 1; week <= weeks; week++) {
@@ -185,6 +254,7 @@ $("#generatePlan").click(function() {
         content += '</tr>';
     }
     content += '</tbody></table>';
+    content += '</body>'
 
     // Create a new window with the content
     let printWindow = window.open('', '_blank');
